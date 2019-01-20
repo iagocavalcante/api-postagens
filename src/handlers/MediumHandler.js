8a2a276393cc6325a00f3b2518a37ab2255b7003
Iago Cavalcante <iagoangelimc@gmail.com>
2019-01-20 20:56:29 -0300

    Transform response in object with text and tag to show in frontend

--- src/handlers/MediumHandler.js
+++ src/handlers/MediumHandler.js
@@ -13,8 +13,9 @@ const showPost = (req, h) => {
   return axios.get(replaceUserAndPostInUrl(req.params.name, req.params.postName))
     .then(response => {
       const cleanPosts = removeTrashFromJson(response.data)
-      const posts = convertToObject(cleanPosts)
-      return h.response(posts)
+      const postObject = convertToObject(cleanPosts)
+      const post = transformInHtml(postObject.payload.value.content.bodyModel.paragraphs)
+      return h.response(post)
     })
     .catch(error => Promise.reject(error.data))
 }
@@ -28,6 +29,22 @@ const convertToObject = ( object ) => JSON.parse(object)
 const replaceUserInUrl = ( name ) => process.env.MEDIUM_HOST_USER.replace('@', `@${name}`)
 
 const replaceUserAndPostInUrl = ( name, postName ) => process.env.MEDIUM_HOST_POST.replace('@', `@${name}`).replace('post', `${postName}`)
+
+const transformInHtml = ( paragraphs ) => 
+  paragraphs.reduce((html, paragraph) => {
+    return html.concat({text: `${paragraph.text}`, tag: checkType(paragraph.type)})
+  }, [])
+
+const checkType = ( type ) => 
+  type === 10 ? '<li>_$_</li>' : 
+    type === 1 ? '<p>_$_</p>' : 
+      type === 3 ? '<h1>_$_</h1>' : 
+        type === 4 ? '<p>_$_</p>' : 
+          type === 11 ? '<embed src="_$_">' : 
+            type === 7 ? '<blockquote>_$_</blockquote>' : 
+              type === 6 ? '<blockquote>_$_</blockquote>' : 
+                type === 13 ? '<h2>_$_</h2>' :
+                  '<p>_$_</p>'
 
 module.exports = {
   list,
