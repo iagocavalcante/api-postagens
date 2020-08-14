32ba7c58b5e67e2728ac07ab7560284b05cff3b4
Iago Cavalcante <iagoangelimc@gmail.com>
2020-08-14 20:34:19 -0300

    fix: ajustando retorno de posts

--- src/handlers/MediumHandler.js
+++ src/handlers/MediumHandler.js
@@ -15,24 +15,19 @@ const showPost = (req, h) => {
 			const cleanPosts = removeTrashFromJson(response.data)
 			const postObject = convertToObject(cleanPosts)
 			const post = transformInHtml(postObject.payload.value.content.bodyModel.paragraphs)
-			return h.response(postObject)
+			return h.response(post)
 		})
 		.catch(error => Promise.reject(error.data))
 }
-
 const transformObjectToArray = ( posts ) => Object.values(posts)
-
 const removeTrashFromJson = ( json ) => json.replace('])}while(1);</x>', '')
-
 const convertToObject = ( object ) => JSON.parse(object)
-
 const replaceUserInUrl = ( name ) => process.env.MEDIUM_HOST_USER.replace('@', `@${name}`)
-
 const replaceUserAndPostInUrl = ( name, postName ) => process.env.MEDIUM_HOST_POST.replace('@', `@${name}`).replace('post', `${postName}`)
 
 const transformInHtml = ( paragraphs ) => 
 	paragraphs.reduce((html, paragraph) => {
-    if (!paragraph.type && paragraph.type === 11) paragraph.text = paragraph.iframe.thumbnailUrl
+		if (!paragraph.type && paragraph.type === 11) paragraph.text = paragraph.iframe.thumbnailUrl
 		return html.concat({text: `${paragraph.text}`, tag: checkType(paragraph.type)})
 	}, [])
 
@@ -43,7 +38,7 @@ const TYPES = {
 	1 : '<p>_$_</p>',
 	3 : '<h1>_$_</h1>',
 	4 : '<p>_$_</p>',
-	11:  '<embed src="_$_">',
+	11:  '<embed src="_$_"/>',
 	7 : '<blockquote>_$_</blockquote>',
 	6 : '<blockquote>_$_</blockquote>',
 	13:  '<h2>_$_</h2>',
